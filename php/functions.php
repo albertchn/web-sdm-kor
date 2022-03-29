28b9a05a5f434a286608ea1d348158f6e86fa3fb
Developer <developer@smkn12jakarta.my.id>
2022-03-29 22:17:18 +0700

    first commit

---
+++ php/functions.php
@@ -0,0 +1,15 @@
+<?php 
+// connection to database
+$conn = mysqli_connect("localhost", "root", "", "pelanggaran_siswa");
+
+// basic query convert to array
+function query($query) {
+    global $conn;
+    $result = mysqli_query($conn, $query);
+    $rows = [];
+    while ( $row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+
+    return $rows;
+}
